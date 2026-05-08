28a6bf438e7301ea7348c3d3fb7bbcb0813f41c5
luming5 <luming5@xiaomi.com>
2026-05-08 08:43:51 +0800

    shsshhh:
    Signed-off-by: luming5 <luming5@xiaomi.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,5 @@
 pluginManagement {
     repositories {
-        maven { url = uri("https://pkgs.d.xiaomi.net:443/artifactory/maven-release-virtual/") }
-        maven { url = uri("https://pkgs.d.xiaomi.net:443/artifactory/maven-remote-virtual/") }
-        maven { url = uri("https://pkgs.d.xiaomi.net:443/artifactory/maven-snapshot-virtual/") }
-        maven { url = uri("https://pkgs.d.xiaomi.net/artifactory/public") }
         // 清华大学开源软件镜像站
         maven { url = uri("https://mirrors.tuna.tsinghua.edu.cn/maven2/") }
         // 阿里巴巴开源软件镜像站
@@ -18,10 +14,6 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven { url = uri("https://pkgs.d.xiaomi.net:443/artifactory/maven-release-virtual/") }
-        maven { url = uri("https://pkgs.d.xiaomi.net:443/artifactory/maven-remote-virtual/") }
-        maven { url = uri("https://pkgs.d.xiaomi.net:443/artifactory/maven-snapshot-virtual/") }
-        maven { url = uri("https://pkgs.d.xiaomi.net/artifactory/public") }
         // 清华大学开源软件镜像站
         maven { url = uri("https://mirrors.tuna.tsinghua.edu.cn/maven2/") }
         // 阿里巴巴开源软件镜像站
